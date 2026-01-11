163186990ed7e4f297ba725ba9f44ee59e03662d
bluvision30-netizen <bluvision30@gmail.com>
2026-01-11 08:35:53 +0100

    Delete src directory

--- src/app/About.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/About.jsx
-export default function About() {
-  return (
-    <div className="container">
-      <h1>À propos de notre boutique</h1>
-      <p>Bienvenue sur E-Shop, votre boutique en ligne d’électronique.</p>
-    </div>
-  );
-}
